feb1b51a180a206c023a794dd669df16503cee56
samuelasensio <75941684+samuelasensio@users.noreply.github.com>
2025-03-10 08:13:58 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,1 @@
-// script.js
 
-document.addEventListener("DOMContentLoaded", function() {
-    const mensaje = "Nos hemos trasladado a otra web de venta de entradas";
-    const mensajeElemento = document.getElementById("mensaje-traslado");
-    const botonTraslado = document.getElementById("boton-traslado");
-
-    // Mostrar mensaje con un retraso de 2 segundos
-    setTimeout(() => {
-        mensajeElemento.textContent = mensaje;
-    }, 2000);
-
-    // Redireccionar al hacer clic en el botón
-    botonTraslado.addEventListener("click", () => {
-        window.location.href = "https://nueva-web-de-entradas.com";
-    });
-});
